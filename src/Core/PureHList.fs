74bc10b9c9fa3938e41bafb138a21f17d75c4964
nemonuri <nemonuri2@gmail.com>
2026-03-31 10:26:46 +0900

    와, 테스트도 통과!

--- src/Core/PureHList.fs
+++ src/Core/PureHList.fs
@@ -1,6 +1,6 @@
 #nowarn "42"
 
-namespace rec Nemonuri.Collections.Heterogeneous
+namespace Nemonuri.Collections.Heterogeneous
 
 open System
 open Nemonuri.Handles
@@ -8,63 +8,39 @@ open Nemonuri.Collections.Heterogeneous.Primitives
 
 [<RequireQualifiedAccess>]
 [<NoEquality; NoComparison>]
-type PureHList<'ctx> =
+type PureHList<'TPred> =
     private
     | Empty
-    | Cons of ctx:'ctx * arrowPtr:nativeint * visitable:IFolderVisitable<'ctx>
+    | Cons of predecessor:'TPred * visitable:IFolderVisitable<'TPred>
 
 module PureHLists = begin
 
-    type private vunit = System.ValueTuple
+    type private Nil = System.ValueTuple
 
-    type private Context<'hd, 'tl> = System.ValueTuple<'hd, PureHList<'tl>>
+    type private Pair<'hd, 'tl> = System.ValueTuple<'hd, PureHList<'tl>>
 
 
-    [<NoEquality; NoComparison>]
-    type private IdentityArrow<'ctx, 'hd, 'tl> = struct
-
-        interface IInArrowPremise<'ctx, Context<'hd, 'tl>> with
-            member x.Apply (source: inref<'ctx>): Context<'hd, 'tl> = 
-                assert ( typeof<'ctx> = typeof<Context<'hd, 'tl>> );
-                RetypeTheory.UnsafeRetype<'ctx,Context<'hd, 'tl>>(&source)
-
-    end
-
-    type private IdentityArrowHandle<'ctx, 'hd, 'tl> = InArrowHandle<'ctx, Context<'hd, 'tl>>
-
-    [<NoEquality; NoComparison>]
-    type private Decons<'ctx, 'hd, 'tl> = struct
-
-        static member TryDeconstruct (source: inref<PureHList<'ctx>>, result: byref<Context<'hd, 'tl>>, error: byref<vunit>): bool = 
-            match source with
-            | PureHList.Empty -> error <- vunit(); false
-            | PureHList.Cons (ctx, arrowPtr, _) -> 
-                let arrowHandle = HandleTheory.UnsafeFromIntPtr<IdentityArrowHandle<'ctx, 'hd, 'tl>>(arrowPtr) in
-                result <- arrowHandle.Apply(&ctx);
-                true
-
-        interface IInRefInductivePremise<PureHList<'ctx>, Context<'hd, 'tl>, vunit> with
-            member x.TryDeconstruct (source, result, error): bool = Decons<'ctx, 'hd, 'tl>.TryDeconstruct(&source, &result, &error)
-
-    end
-
-    let empty: PureHList<vunit> = PureHList.Empty
+    let empty: PureHList<Nil> = PureHList.Empty
 
     let isEmpty (l: PureHList<_>) = l.IsEmpty
 
-    let tryDeconsV (l: PureHList<_>) =
-        let mutable result = Unchecked.defaultof<_>
-        let mutable error = Unchecked.defaultof<_>
-        let ok = Decons<_,_,_>.TryDeconstruct(&l, &result, &error)
-        if ok then ValueSome result else ValueNone
+    let private tryPredV (l: PureHList<_>) =
+        match l with
+        | PureHList.Empty -> ValueNone
+        | PureHList.Cons (pred, _) -> ValueSome pred
+
+    let private toPair (l: PureHList<Pair<'hd, 'tl>>) =
+        match tryPredV l with
+        | ValueNone -> failwith "Unreachable"
+        | ValueSome pred -> pred
     
-    let cons (hd: 'hd) (l: PureHList<'tl>) =
-        let arrowPtr = InArrowTheory.ToHandle<_,_,IdentityArrow<Context<'hd, 'tl>, 'hd, 'tl>>().ToIntPtr() in
-        PureHList.Cons (struct (hd, l), arrowPtr, PureHLists.Visitable<'hd,'tl>.Instance)
+    let head l = match toPair l with | struct (hd, _) -> hd
+
+    let tail l = match toPair l with | struct (_, tl) -> tl
     
     let private fold_core (folder: IFolder<'state>) (acc: 'state) (l: PureHList<'ctx>) =
         match l with
-        | PureHList.Cons (ctx, _, visitable) -> visitable.Accept(folder, acc, ctx)
+        | PureHList.Cons (ctx, visitable) -> visitable.Accept(folder, acc, ctx)
         | _ -> acc
             
 
@@ -74,17 +50,24 @@ module PureHLists = begin
 
         static member Instance : Visitable<'hd, 'tl> = Visitable<_,_>()
 
-        static member Accept<'state> (folder: IFolder<'state>, acc: 'state, ctx: Context<'hd,'tl>): 'state = 
+        static member Accept<'state> (folder: IFolder<'state>, acc: 'state, ctx: Pair<'hd,'tl>): 'state = 
             let struct (hd, tl) = ctx in
             let nextAcc = folder.Step<'hd>(acc, hd)
             fold_core folder nextAcc tl
 
-        interface IFolderVisitable<Context<'hd, 'tl>> with
+        interface IFolderVisitable<Pair<'hd, 'tl>> with
             member _.Accept (folder, acc, ctx) = Visitable<'hd, 'tl>.Accept(folder, acc, ctx)
 
     end
 
-    let fold folder (seed: 'state) (l: PureHList<'ctx>) = fold_core folder seed l
+    let cons (hd: 'hd) (l: PureHList<'tl>) =
+        PureHList.Cons (struct (hd, l), Visitable<'hd,'tl>.Instance)
+
+    let fold folder seed l = fold_core folder seed l
+
+    let private folderForLength = { new IFolder<int> with member _.Step (acc: int, _: 'T): int = acc + 1 }
+
+    let length l = fold folderForLength 0 l
 
 
 end
--- src/test/Core/PureHLists.fs
+++ src/test/Core/PureHLists.fs
@@ -7,11 +7,61 @@ open Nemonuri.Collections.Heterogeneous.Primitives
 open Nemonuri.Collections.Heterogeneous.PureHLists
 
 
-(* TODO: 좀 더 제대로 된 테스트 만들기 *)
+module private Fixtures = begin
 
+    let list1 =
+        empty 
+        |> cons 3 
+        |> cons -2.5 
+        |> cons "0" 
+        |> cons "1" 
+        |> cons 4u
+
+    let list2 =
+        empty
+        |> cons ()
+
+end
+
+
+(* TODO: 좀 더 제대로 된 이름 짓기 *)
 [<Fact>]
 let Test1() =
-    let l = empty |> cons 3 |> cons -2.5 |> cons "0" |> cons "1" |> cons 4u in
     let folder = QuickHLists.toIntFolder { new QuickHLists.IPredicate with member _.Invoke (x: 'T): bool = QuickHLists.isLesserThan 0 x } in
-    let actual = fold folder 0 l in
+    let actual = fold folder 0 Fixtures.list1 in
     Assert.Equal(2, actual)
+
+[<Fact>]
+let ``empty is empty``() =
+    let actual = empty |> isEmpty
+    Assert.True(actual)
+
+[<Fact>]
+let ``(length empty) is 0``() =
+    let actual = (length empty)
+    Assert.Equal(0, actual)
+
+[<Fact>]
+let ``(length Fixtures.list1) is 5``() =
+    let actual = (length Fixtures.list1)
+    Assert.Equal(5, actual)
+
+[<Fact>]
+let ``(head Fixtures.list1) is (uint32 4)``() =
+    let actual = (head Fixtures.list1)
+    Assert.Equal((uint32 4), Assert.IsType<uint32>(actual))
+
+[<Fact>]
+let ``length of tail is (length of original - 1) - Test1``() =
+    let original = Fixtures.list1
+    let expected = (length original) - 1
+    let actual = (tail original |> length)
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``length of tail is (length of original - 1) - Test2``() =
+    let original = Fixtures.list2
+    let expected = (length original) - 1
+    let actual = (tail original |> length)
+    Assert.Equal(expected, actual)
+
